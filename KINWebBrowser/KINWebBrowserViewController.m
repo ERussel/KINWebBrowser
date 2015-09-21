de5933e9f0e35ed7f46466d56ceb72400bb698c9
Russel <emkil.russel@gmail.com>
2015-09-21 11:19:59 +0500

    [fix] iOS 9 warnings

--- KINWebBrowser/KINWebBrowserViewController.m
+++ KINWebBrowser/KINWebBrowserViewController.m
@@ -578,7 +578,7 @@ static void *KINWebBrowserContext = &KINWebBrowserContext;
 
 #pragma mark - Interface Orientation
 
-- (NSUInteger)supportedInterfaceOrientations {
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
     return UIInterfaceOrientationMaskAllButUpsideDown;
 }
 
